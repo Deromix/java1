adba2f05cde0e50b73bf8e3c90dbb4426da03813
Elizabeth <denisshishmakov@gmail.com>
2022-07-05 17:12:47 +0400

    HelloWorld start

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -4,11 +4,11 @@ public class ReverseDigits {
     public static int reverseDigits(int number){
         int reverse = number % 10 ;
         System.out.print(reverse);
-        reverse = number / 10;
-        reverse = reverse % 10;
+        number = number  / 10;
+        reverse = number % 10;
         System.out.print(reverse);
-        reverse = number / 100;
-        reverse = reverse % 10;
+        number = number  / 10;
+        reverse = number % 10;
         System.out.println(reverse);
         return reverse;
     }
