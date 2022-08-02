d0e03c2f588eb21ca937fc5ae0239a33839eae6d
Elizabeth <denisshishmakov@gmail.com>
2022-08-02 14:24:10 +0400

    HelloWorld start

--- src/ru/progwards/java1/lessons/inheritance/TimeZone.java
+++ src/ru/progwards/java1/lessons/inheritance/TimeZone.java
@@ -12,7 +12,6 @@ public class TimeZone {
     public TimeZone(int hours, int minutes) {
         this.hours = hours;
         this.minutes = minutes;
-
     }
 }
 
@@ -48,20 +47,18 @@ public class TimeZone {
                 sdvigk2 = 0;
             else
                 sdvigk2 = Math.abs(k2.hours * 60 * 60 + k2.minutes * 60);
-
+            if(zone.hours < 0)
+                return super.secondsBetween(time) - Math.abs(sdvigk1 + sdvigk2);
             return super.secondsBetween(time) + Math.abs(sdvigk1 + sdvigk2);
         }
 
 
 
         public static void main(String[] args) {
-
-            ZonedTime t1 = new ZonedTime(2,4,3);
-            ZonedTime t2 = new ZonedTime(1,2,3);
-            System.out.println(t1.secondsBetween(t2));
-            ZonedTime zt1 = new ZonedTime(18, 39, 26, new TimeZone(1));
+            ZonedTime zt1 = new ZonedTime(18, 39, 26, new TimeZone(-1, 2));
             ZonedTime zt2 = new ZonedTime(20, 49, 57, new TimeZone(3));
             System.out.println(zt1.secondsBetween(zt2));
+
         }
     }
 
