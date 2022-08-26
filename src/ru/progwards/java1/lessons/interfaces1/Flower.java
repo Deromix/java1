4481d8381102c3253767a2924283851103f9fa1d
Elizabeth <denisshishmakov@gmail.com>
2022-08-26 16:08:55 +0400

    HelloWorld start

--- src/ru/progwards/java1/lessons/interfaces1/Flower.java
+++ src/ru/progwards/java1/lessons/interfaces1/Flower.java
@@ -1,9 +1,33 @@
 package ru.progwards.java1.lessons.interfaces1;
 
-public class Flower implements IColor{
-    Color white = Color.WHITE;
+public class Flower extends Animal{
+    public Flower(String name) {
+        super(name);
+    }
+
     @Override
-    public Animal.Color getColor() {
+    public String kind() {
+        return null;
+    }
+
+    @Override
+    public String say() {
+        return null;
+    }
+
+    @Override
+    public FoodKind getFoodKind() {
+        return null;
+    }
+
+    @Override
+    public double getFoodCoeff() {
+        return 0;
+    }
+
+    @Override
+    public Color getColor() {
+        Color white = Color.WHITE;
         white = Color.valueOf("WHITE");
         return white;
     }
