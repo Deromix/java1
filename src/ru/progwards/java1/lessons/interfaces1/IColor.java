a333334277618d8f7e39ef9e007169028af28746
Elizabeth <denisshishmakov@gmail.com>
2022-09-03 16:29:07 +0400

    HelloWorld start

--- src/ru/progwards/java1/lessons/interfaces1/IColor.java
+++ src/ru/progwards/java1/lessons/interfaces1/IColor.java
@@ -1,6 +1,12 @@
 package ru.progwards.java1.lessons.interfaces1;
 
     public interface IColor {
+        public enum Color {
+            GRAY,
+            WHITE,
+            RED,
+            BEIGE
+        }
         Color getColor();
     }
 
