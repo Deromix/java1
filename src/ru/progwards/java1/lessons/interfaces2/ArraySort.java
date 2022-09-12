8a4382e7fa40aae53531b8f15e0369439dd7290c
Elizabeth <denisshishmakov@gmail.com>
2022-09-12 14:00:52 +0400

    HelloWorld start

--- src/ru/progwards/java1/lessons/interfaces2/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces2/ArraySort.java
@@ -1,26 +1,46 @@
 package ru.progwards.java1.lessons.interfaces2;
 
-public class ArraySort {
+public class ArraySort implements CompareWeight {
 
-    public static void sort(int[] a){
-        int i;
-        int j;
-        for(i = 1; i < a.length; i++ ){
-            for(j = 0; j < a.length; j++){
-            int l = 0;
-            if(a[j] > a[i]) {
-                l = a[j];
-                a[j] = a[i];
-                a[i] = l;
-            }
+    @Override
+    public CompareResult compareWeight(CompareWeight smthHasWeight) {
+        CompareResult less = CompareResult.LESS;
+        less = CompareResult.valueOf("LESS");
+        CompareResult equal = CompareResult.EQUAL;
+        equal = CompareResult.valueOf("EQUAL");
+        CompareResult greater = CompareResult.GREATER;
+        greater = CompareResult.valueOf("GREATER");
+        if(this.getWeight() < smthHasWeight.getWeight())
+            return less;
+        if(this.getWeight() > smthHasWeight.getWeight())
+            return greater;
+        else
+            return equal;
+    }
+
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    public static void sort(CompareWeight[] a) {
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                CompareWeight l;
+                if (a[j].getWeight() > a[i].getWeight()) {
+                    l = a[j];
+                    a[j] = a[i];
+                    a[i] = l;
+                }
             }
         }
     }
 
+
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5};
         int[] b = {32,21,123,234,135,326,3,76,12,11,9};
-        sort(b);
         for(int value: b){
             System.out.println(value);
         }
