8fd91e56fe3f3829b78569018e4a7638d1253127
Elizabeth <denisshishmakov@gmail.com>
2022-09-12 13:54:06 +0400

    HelloWorld start

---
+++ src/ru/progwards/java1/lessons/interfaces2/ArraySort.java
@@ -0,0 +1,28 @@
+package ru.progwards.java1.lessons.interfaces2;
+
+public class ArraySort {
+
+    public static void sort(int[] a){
+        int i;
+        int j;
+        for(i = 1; i < a.length; i++ ){
+            for(j = 0; j < a.length; j++){
+            int l = 0;
+            if(a[j] > a[i]) {
+                l = a[j];
+                a[j] = a[i];
+                a[i] = l;
+            }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        int[] b = {32,21,123,234,135,326,3,76,12,11,9};
+        sort(b);
+        for(int value: b){
+            System.out.println(value);
+        }
+    }
+}
